7e4f6ba675a5141c5ccef3c6c20f94df795b4444
Lane Lawley <lxl5734@rit.edu>
2014-02-11 13:02:06 -0500

    More informative name for Python rendering script

--- src/gentrain.sh
+++ src/gentrain.sh
@@ -3,5 +3,5 @@
 if [ ! -d "../img" ]; then mkdir ../img; fi
 if [ ! -d "../img/training" ]; then mkdir ../img/training; fi
 
-python drawimg.py < ../txt/joyo.txt 
+python renderdataset.py < ../txt/joyo.txt 
 ./trainer ../txt/db.txt ../img/training/
